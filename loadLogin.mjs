707d48318388fd7fa95dde6ef1ffcb060782cdf9
LiliJohnson <90824333+LiliJohnson@users.noreply.github.com>
2021-09-21 13:55:29 +0500

    CU-cqgja0[Complete]Status change attempt 6

--- loadLogin.mjs
+++ loadLogin.mjs
@@ -1,18 +1,18 @@
 import fetch from 'node-fetch';
 globalThis.fetch = fetch;
 
-let callEveryXMilliSeconds = 40;
-let url = 'https://api.colonise.com/api/Account';
-let tsAppToken = '82402-64984162582de14f2b98a4a6958146fc46ada8b4';
-// let tsAppToken = '82404-38231cb555814121d8b9aabc911747cccedaba1e';
+let callEveryXMilliSeconds = 5;
+let url = 'https://api.avaza.com/api/Account';
+let tsAppToken1 = '425304-58d279b275020f9e90376e2fe9f4f90cba781bd0';
+let tsAppToken2 = '425295-43d8a5b29983a87cab6cb553366e0732072ead8e';
 let numberOfTimes = 1;
-let limit = 1010;
+let limit = 10100;
 
 const runScript = () => {
 	if (numberOfTimes > limit) {callAPIs && clearInterval(callAPIs); return;}
 	fetch(url, {method: 'GET', headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${tsAppToken}`}})
-		.then(response => response.json())
-		.then(data => {
+	.then(response => response.json())
+	.then(data => {
 			console.log('#', numberOfTimes + '/' + limit);
 			numberOfTimes += 1;
 			console.log('Success:', data.CurrentServerTimeISO ? data.CurrentServerTimeISO : data);
